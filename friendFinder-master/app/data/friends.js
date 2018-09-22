8398cd3e949bdf9acfd5d810a7b75b4fc7d676eb
Brent Shierk <bshierk1996@gmail.com>
2018-09-22 16:54:31 -0400

    hw

---
+++ friendFinder-master/app/data/friends.js
@@ -0,0 +1,21 @@
+
+
+var tableArray = [
+	{
+		name: 'brent shierk',
+		photo: 'https://i.ytimg.com/vi/fSSHN0eReRs/maxresdefault.jpg',
+		scores: ["1","1","4","1","1","1","2","2","1","1"]
+	},
+	{
+		name: 'bob',
+		photo: 'https://i.chzbgr.com/full/9064895744/hD8E6FA43/',
+		scores: ["3","1","3","3","3","1","2","3","3","1"]
+	},
+	{
+		name: 'bread',
+		photo: 'https://i.kym-cdn.com/entries/icons/original/000/022/939/kHPxj2F.jpg',
+		scores: ["5","5","4","4","5","5","5","5","4","5"]
+	}
+];
+
+module.exports = tableArray;
